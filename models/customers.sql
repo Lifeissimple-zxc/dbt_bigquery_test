c5236917af8ffe784f0b799f164070486a4f0d91
Makario <mararkarp@gmail.com>
2022-05-24 23:40:52 +0300

    Added sources to models yml

--- models/customers.sql
+++ models/customers.sql
@@ -17,7 +17,8 @@ customer_orders as (
 
         min(order_date) as first_order_date,
         max(order_date) as most_recent_order_date,
-        count(order_id) as number_of_orders
+        count(order_id) as number_of_orders,
+        sum(payment_amount) as total_payment_amount
 
     from orders
 
@@ -33,7 +34,8 @@ final as (
         customers.last_name,
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
-        coalesce(customer_orders.number_of_orders, 0) as number_of_orders
+        coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
+        coalesce(customer_orders.total_payment_amount, 0) as total_payment_amount
 
     from customers
 
